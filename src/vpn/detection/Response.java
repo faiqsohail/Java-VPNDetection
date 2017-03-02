bc623c0be09f06b9de6e77012e7952fbed483b76
HiddenMotives <HiddenMotives@users.noreply.github.com>
2017-03-02 10:41:01 -0700

    first commit

---
+++ src/vpn/detection/Response.java
@@ -0,0 +1,40 @@
+package vpn.detection;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Response {
+    public String status;
+    public String msg;
+    
+    @SerializedName("package")
+    public String getPackage;
+    
+    public String remaining_requests;
+    public String ipaddress;
+    
+    @SerializedName("host-ip")
+    public boolean hostip;
+    
+    public String org;
+    
+    public CS country;
+    public CS subdivision;
+    
+    public String city;
+    public String postal;
+    
+    public latlon location;
+    
+    public class CS {
+        public String name;
+        public String code;
+    }
+    
+    public class latlon {
+        public double lat;
+        
+        @SerializedName("long")
+        public double lon;
+    }
+    
+}
